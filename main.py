e391226d92b842401792836ffa173d0c6a653c8f
Isuru Walpola <iwalpola@studiow.cf>
2019-02-18 22:35:37 +0530

    gui

--- main.py
+++ main.py
@@ -6,11 +6,13 @@
   All Rights Reserved
 
 """
+import sys
 import modbusdevice as modbus
-from datetime import datetime
-from datetime import time as dtime
-import pyglet
-import time
+from datetime import datetime, time
+if sys.version_info[0] >= 3:
+	import PySimpleGUI as sg
+else:
+	import PySimpleGUI27 as sg
 
 """
 initiate deviceCollection dictionary
@@ -27,13 +29,14 @@ deviceCollection = {
 sumkW = 0
 sumDailyDelta = 0
 sumMonthlyDelta = 0
+sumkWh = 0
 
-"""Set updation frequency"""
-update_interval = 30.0
+"""Set updation frequency in seconds"""
+update_interval = 60
 
 """set program start and end time"""
-prog_start = dtime(6,0,0)
-prog_end = dtime(18,0,0)
+prog_start = time(6,0,0)
+prog_end = time(21,0,0)
 
 """ query device array """
 def query_devices():
@@ -46,34 +49,47 @@ def query_devices():
 			global sumDailyDelta
 			global sumMonthlyDelta
 			global sumkW
+			global sumkWh
+			sumkW = 0
+			sumkWh = 0
+			sumDailyDelta = 0
+			sumMonthlyDelta = 0
 			sumDailyDelta += deviceCollection[device].daily_delta
 			sumMonthlyDelta += deviceCollection[device].monthly_delta
 			sumkW += deviceCollection[device].kW
+			sumkWh += deviceCollection[device].kWh
 
 def update_display():
-	print "display curently undefined"
-	print "Current Generation (kW): " + str(sumkW)
-	print "Total Monthly Energy (kWh): " + str(sumMonthlyDelta)
-	print "Total Daily Energy (kWh): " + str(sumDailyDelta)
-"""main script"""
-"""Run once"""
-#Setup the window
-#window = pyglet.window.Window()
-#image = pyglet.resource.image('kitten.png')
-#def on_draw():
-#    window.clear()
-#    image.blit(0, 0)
-#pyglet.app.run()
+	# print "display curently undefined"
+	# print "Current Generation (kW): " + str(sumkW)
+	# print "Total Monthly Energy (kWh): " + str(sumMonthlyDelta)
+	# print "Total Daily Energy (kWh): " + str(sumDailyDelta)
+	print "update display"
+	window.FindElement('_ckW_').Update(str(sumkW))
+	window.FindElement('_dkWh_').Update(str(sumDailyDelta))
+	window.FindElement('_mkWh_').Update(str(sumMonthlyDelta))
+	window.FindElement('_ltkWh_').Update(str(sumkWh))
 
-"""Scheduling loop every 10 minutes"""
-starttime=time.time()
+layout = [[sg.Text('Latest Data')],
+		[sg.Text('Current Generation (kW):'), sg.Text('', key='_ckW_')],
+		[sg.Text('Total Daily Energy (kWh):'), sg.Text('', key='_dkWh_')],
+		[sg.Text('Total Monthly Energy (kWh):'), sg.Text('', key='_mkWh_')],
+		[sg.Text('Lifetime Energy Generation (kWh):'), sg.Text('', key='_ltkWh_')],
+		[sg.Button('Restart'), sg.Button('Exit')]]
+
+window = sg.Window('Ananda College Solar Monitor').Layout(layout)
+"""Run once"""
+event, values = window.Read(timeout=1)
+query_devices()
+update_display()
+"""Scheduling loop every n seconds"""
 while True:
 	time_now = datetime.now().time()
-	if time_now>prog_start and time_now<prog_end:
-		sumkW = 0
-		sumDailyDelta = 0
-		sumMonthlyDelta = 0
+	event, values = window.Read(timeout=update_interval*1000)
+	if event == 'Exit':
+		break
+	elif time_now>prog_start and time_now<prog_end:
 		query_devices()
 		update_display()
 	else: print "doing nothing"
-	time.sleep(update_interval - ((time.time() - starttime) % update_interval))
+window.Close()
