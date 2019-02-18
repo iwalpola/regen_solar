20d808a34c3de3f5888d770559e48182954be2b7
Isuru Walpola <iwalpola@studiow.cf>
2019-02-18 16:02:09 +0530

    second wave

--- main.py
+++ main.py
@@ -7,32 +7,59 @@
 
 """
 import modbusdevice as modbus
-from datetime import datetime, time
+from datetime import datetime
+from datetime import time as dtime
+import pyglet
+import time
 
 """
 initiate deviceCollection dictionary
 """
+"""Should fake? put 0 for no, 1 for yes"""
+fake = 0
 
 deviceCollection = {
-	"d1" : modbus.Device(a=1,i='127.0.0.1', name = 'd1'),
-	"d2" : modbus.Device(a=1,i='127.0.0.1', name = 'd2'),
-	"d3" : modbus.Device(a=1,i='127.0.0.1', name = 'd3')
+	"d1" : modbus.Device(a=1,i='127.0.0.1', name = 'd1', fake = fake),
+	"d2" : modbus.Device(a=1,i='127.0.0.1', name = 'd2', fake = fake),
+	"d3" : modbus.Device(a=1,i='127.0.0.1', name = 'd3', fake = fake)
 }
+sumDailyDelta = 0
+sumMonthlyDelta = 0
 
-""" instantiate persistant storage """
-persistance = helpers.Persistance()
-
-""" query and update persistant data """
-def refresh_persistance(xx):
-	if xx.last_set_time.day<datetime.now().day: #check if valid and update if necessary
-		newinitvals=[200,200,200] #replace withfunction to read
-		xx.update(initdata=newinitvals)
-	#print "finished refreshing"
-	#print xx.data
+"""set program start and end time"""
+prog_start = dtime(6,0,0)
+prog_end = dtime(18,0,0)
 
 """ query device array """
 def query_devices(deviceCollection):
 	for device in deviceCollection:
-		deviceCollection[device].read()
+		if not deviceCollection[device].online_status
+			deviceCollection[device].connect()
+		if deviceCollection[device].online_status
+			deviceCollection[device].read()
+			deviceCollection[device].calcdelta()
+		if deviceCollection[device].online_status:
+			sumDailyDelta += deviceCollection[device].daily_delta
+			sumMonthlyDelta += deviceCollection[device].monthly_delta
 
+def update_display():
+	print "currently empty"
 
+"""main script"""
+"""Run once"""
+#Setup the window
+#window = pyglet.window.Window()
+#image = pyglet.resource.image('kitten.png')
+#def on_draw():
+#    window.clear()
+#    image.blit(0, 0)
+#pyglet.app.run()
+
+"""Scheduling loop every 10 minutes"""
+starttime=time.time()
+while True:
+	time_now = datetime.now().time()
+	if time_now>prog_start and time_now<prog_end
+		query_devices(deviceCollection)
+		update_display()
+	time.sleep(600.0 - ((time.time() - starttime) % 600.0))
--- modbusdevice.py
+++ modbusdevice.py
@@ -1,44 +1,59 @@
 import sunspec.core.client as client
 import json
-from datetime import datetime, time
+from datetime import datetime
 
 """ Device class represents and manipulates Sunspec Devices on the network. """
 
 class Device(object):
 
-	def __init__(self, a=1, i='', name='device_friendly_name'):
+	def __init__(self, a=1, i='', name='device_friendly_name', fake=0):
 		self.a = a
+		self.fake = fake
 		self.i = i
 		self.P = 502
 		self.T = 2.0
 		self.name = name
 		self.kW = 0
 		self.kWh = 0
+		self.daily_delta = 0
+		self.monthly_delta = 0
 		self.persistance = {"monthly_initial" : {"last_set": datetime.now(), "value": 0}, "daily_initial": {"last_set": datetime.now(), "value": 0}}
+		self.connect()
+		self.read()
+		self.retrieve()
+		self.calcdelta()
 
 	def connect():
-		""" Create a new inverter device """
-		self.online_status = 0
-		try:
-			self.connection = client.SunSpecClientDevice(client.TCP, self.a, ipaddr=self.i, ipport=self.P, timeout=self.T)
+		if fake:
 			self.online_status = 1
-		except client.SunSpecClientError, e:
-			print 'Error: %s' % (e)
+		else:
+			""" Create a new inverter device """
 			self.online_status = 0
+			try:
+				self.connection = client.SunSpecClientDevice(client.TCP, self.a, ipaddr=self.i, ipport=self.P, timeout=self.T)
+				self.online_status = 1
+			except client.SunSpecClientError, e:
+				print 'Error: %s' % (e)
+				self.online_status = 0
 
 	def read(self):
-		if self.online_status:
-			"""
+		if fake:
+			self.connection.inverter.read()
+			self.kW = (2400)/1000
+			self.kWh = (25089)/1000
+		elif self.online_status:
 			try:
 				self.connection.inverter.read()
-				self.kW = self.connection.inverter
-				self.kWh = self.connection.inverter
+				self.kW = (self.connection.inverter.points[14])/1000
+				self.kWh = (self.connection.inverter.points[24])/1000
 			except e:
 				self.online_status = 0
-			"""
-		else:
-			#try connecting again
 
+	def calcdelta(self):
+		if self.online_status:
+			self.monthly_delta = self.kWh - self.persistance["monthly_initial"]["value"]
+			self.daily_delta = self.kWh - self.persistance["daily_initial"]["value"]
+	
 	def retrieve(self):
 		f = open(self.name+'.data','r')
 		self.persistance = json.load(f)
@@ -53,7 +68,7 @@ class Device(object):
 			self.persist(monthly=1)
 
 	def persist(self, daily = 0, monthly = 0):
-		if self.kWh ==0:
+		if self.kWh==0 and self.online_status:
 			print "zero kWh value not allowed to persist"
 			return
 		now = datetime.now()
